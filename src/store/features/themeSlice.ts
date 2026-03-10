4a09917e6b53f17e0572bc00323d658e367f2738
Suraj S Kumar <your-github-email@example.com>
2026-03-10 13:57:27 +0530

    dark mode fix to localstorage

--- src/store/features/themeSlice.ts
+++ src/store/features/themeSlice.ts
@@ -2,10 +2,16 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 interface Theme {
   color: "light" | "dark";
 }
+const themeStorageLocal=():"light"|"dark"=>{
+  const theme=localStorage.getItem("theme");
+  return theme==="dark"?"dark":"light"
+
+}
 
 const initialState: Theme = {
-  color: "light",
+  color: themeStorageLocal(),
 };
+
 
 const themeSlice = createSlice({
   name: "theme",
@@ -13,10 +19,12 @@ const themeSlice = createSlice({
   reducers: {
     toggle: (state) => {
       state.color = state.color === "light" ? "dark" : "light";
+      localStorage.setItem("theme",state.color)
     },
 
     setMode: (state, action: PayloadAction<"light" | "dark">) => {
       state.color = action.payload;
+      localStorage.setItem("theme",action.payload)
     },
   },
 });
